2ab0baf7398e14d8e31e7f8baf5d0493b9718ef3
Gustavo_FM <a19gustavofm@iessanclemente.net>
2021-11-02 23:17:53 +0100

    CambioEj26

--- Ejercicio-026/scripts/myscript.js
+++ Ejercicio-026/scripts/myscript.js
@@ -20,9 +20,34 @@
 
 
 /*//date llamar a julian*/
-let fecha
+let fechaObj
+//let fecha
+//let re
+let mes
+let mes1
+let dia
 do{
-    fecha=prompt("Dame tu fecha de nacimiento (dd-mm-aaaa")
-}while(isNaN(fecha)==false)
+    fecha=prompt("Dame tu fecha de nacimiento (aaaa,mm,dd)")
+    re = new RegExp('^[0-9]{4},[0-1]{1}[0-9]{1},[0-3]{1}[0-9]{1}$')
+    fechaObj=new Date(fecha)
+    mes=fechaObj.getMonth()
+    dia=fechaObj.getDate()
+    mes1=mes+1
 
-document.write(fecha)
+}while((re.test(fecha))==false||isNaN(mes)||isNaN(dia))
+
+//fecha1=new Date(fecha)
+
+let año=fechaObj.getFullYear()
+let suma =año+mes1+dia
+let suma1=suma.toString()
+//let resultado=[]
+let total=0
+for(let i=0; i<suma1.length;i++){
+    e=suma1.charAt(i)
+    e=parseInt(e)
+    total+=e
+    //resultado.push(e). Podría haber hecho un array y luego recorrerlo y sumarlo. 
+    //Esto se puede mejorar con la fncion forech creo. lo debo mirar.
+}
+document.write(`Como has nacido el ${dia} del ${mes1} del año ${año} (con formato toDateStrin: ${fechaObj.toDateString()}, con formato ISOString: ${fechaObj.toISOString()} y con toString ${fechaObj.toString()}), tu número de la suerte es ${total}`) 
